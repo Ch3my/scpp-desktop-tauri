0419ab4ea0ca3a70a62d7216f9cffbff93c99054
Ch3my <joseconchan1@gmail.com>
2025-01-28 09:12:15 -0300

    update to tailwind v4

--- src/components/ScreenTitle.tsx
+++ src/components/ScreenTitle.tsx
@@ -14,7 +14,7 @@ interface ScreenTitleProps {
 
 const ScreenTitle: React.FC<ScreenTitleProps> = ({ title }) => {
     return (
-        <header className="flex shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-10">
+        <header className="flex shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-10">
             <div className="flex items-center gap-2">
                 <SidebarTrigger className="-ml-1" />
                 <Separator orientation="vertical" className="mr-2 h-4" />
