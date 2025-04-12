e868c581ab56d551fc6a3a7a3db9ec461d8ae96a
Ch3my <joseconchan1@gmail.com>
2025-04-12 15:52:00 -0400

    Fix bug. Datepicker add two elements on Tab
    When tabbing DatePicker had two elements resolved using span in PopoverTrigger, not ideal but works for now

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -5,16 +5,16 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
 } from "@/components/ui/popover";
 import { DateTime } from "luxon";
 import { useEffect } from "react";
 
 interface DatePickerProps {
-  value?: DateTime | undefined;
-  onChange: (date: DateTime | undefined) => void;
+    value?: DateTime | undefined;
+    onChange: (date: DateTime | undefined) => void;
 }
 
 export const DatePicker: React.FC<DatePickerProps & { className?: string }> = ({ value, onChange, className }) => {
@@ -33,25 +33,26 @@ export const DatePicker: React.FC<DatePickerProps & { className?: string }> = ({
 
     return (
         <Popover modal>
-            <PopoverTrigger>
-                <Button
-                    variant={"outline"}
-                    className={cn(
-                        "w-full justify-start text-left font-normal",
-                        !selectedDate && "text-muted-foreground",
-                        className
-                    )}
-                >
-                    <CalendarIcon />
-                    {selectedDate ? selectedDate.toFormat("DDD") : <span>Pick a date</span>}
-                </Button>
+            <PopoverTrigger asChild>
+                <span>
+                    <Button
+                        variant={"outline"}
+                        className={cn(
+                            "w-full justify-start text-left font-normal",
+                            !selectedDate && "text-muted-foreground",
+                            className
+                        )}
+                    >
+                        <CalendarIcon />
+                        {selectedDate ? selectedDate.toFormat("DDD") : <span>Elige una Fecha</span>}
+                    </Button>
+                </span>
             </PopoverTrigger>
             <PopoverContent className="w-auto p-0">
                 <Calendar
                     mode="single"
                     selected={selectedDate?.toJSDate()}
                     onSelect={handleSelect}
-                    initialFocus
                 />
             </PopoverContent>
         </Popover>
